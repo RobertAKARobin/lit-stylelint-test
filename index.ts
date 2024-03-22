7adb3dbce452acefd0553ad61b9f97b388d1b3d7
RobertAKARobin <contact@robertakarobin.com>
2024-03-22 09:42:45 -0500

    add stylelint

--- index.ts
+++ index.ts
@@ -3,19 +3,19 @@ import {customElement, property} from 'lit/decorators.js';
 
 @customElement('simple-greeting')
 export class SimpleGreeting extends LitElement {
-  // Define scoped styles right with your component, in plain CSS
-  static styles = css`
-    :host {
-      color: blue;
-    }
-  `;
+	// Define scoped styles right with your component, in plain CSS
+	static styles = css`
+	:host {
+		color: blue;
+	}
+	`;
 
-  // Declare reactive properties
-  @property()
-  name?: string = 'World';
+	// Declare reactive properties
+	@property()
+	name?: string = 'World';
 
-  // Render the UI as a function of component state
-  render() {
-    return html`<p>Hello, ${this.name}!</p>`;
-  }
+	// Render the UI as a function of component state
+	render() {
+		return html`<p>Hello, ${this.name}!</p>`;
+	}
 }
